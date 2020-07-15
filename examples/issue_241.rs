1a8be80d627312b2ff766e3ec40442615facb31e
Nick Taylor <nick@nicktaylor.ca>
2020-07-15 17:07:12 +0300

    add issue_241 example as in the issue report

---
+++ examples/issue_241.rs
@@ -0,0 +1,28 @@
+use async_std::task;
+use futures::StreamExt;
+use ipfs::{IpfsOptions, Types, UninitializedIpfs};
+
+fn main() {
+    env_logger::init();
+    let options = IpfsOptions::<Types>::default();
+
+    task::block_on(async move {
+        println!("IPFS options: {:?}", options);
+        let (ipfs, future) = UninitializedIpfs::new(options).await.start().await.unwrap();
+        task::spawn(future);
+
+        // Subscribe
+        let topic = "test1234".to_owned();
+        let mut subscription = ipfs.pubsub_subscribe(topic.clone()).await.unwrap();
+
+        ipfs.pubsub_publish(topic.clone(), vec![41, 41])
+            .await
+            .unwrap();
+        while let Some(message) = subscription.next().await {
+            println!("Got message: {:?}", message)
+        }
+
+        // Exit
+        ipfs.exit_daemon().await;
+    })
+}
